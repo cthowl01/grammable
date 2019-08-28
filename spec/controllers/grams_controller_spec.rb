69f2477d42608d27c4af105b79cbba38bedacb64
Chris Howle <cthowle@gmail.com>
2019-08-28 17:59:54 +0000

    Lesson 12

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
 
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "grams#index action" do
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end 
+  end
+
 
   describe "grams#new action" do
     it "should require users to be logged in" do
@@ -18,12 +31,11 @@ RSpec.describe GramsController, type: :controller do
     it "should successfully show the new form" do
       user = FactoryBot.create(:user)
       sign_in user
-
-
       get :new
       expect(response).to have_http_status(:success)
     end
   end
+
 
   describe "grams#create action" do
 
@@ -36,14 +48,12 @@ RSpec.describe GramsController, type: :controller do
       user = FactoryBot.create(:user)
       sign_in user
 
-
-      post :create, params: { gram: { message: 'Hello!'} }
+      post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
       expect(gram.user).to eq(user)
-      end
     end
 
     it "should properly deal with validation errors" do
@@ -51,10 +61,10 @@ RSpec.describe GramsController, type: :controller do
       sign_in user
 
       gram_count = Gram.count
-      post :create, params: { gram: { message: ''} }
+      post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
       expect(gram_count).to eq Gram.count
     end
 
+  end
 end
